167d9226096274fa8bc2faeac8f7d1383d42082a
sm13g21 <92553380+sm13g21@users.noreply.github.com>
2021-10-19 14:28:56 +0100

    Sorted Out Variables
    Variables now follow the correct naming convention (Hopefully they all do).
    I mainly just changed capitol letters to lower case at the front unless its the class object variable.
    Also I realised I could just remove the URL name grabber as it just gets the original user input.

--- Challange1.java
+++ Challange1.java
@@ -15,31 +15,31 @@ public class Main {
         Main BigBoi = new Main();  // Initialising a new class object Called BigBoi
 
         System.out.println("Please Enter the Persons Email ID");
-        String GrabbedNameAdd = BigBoi.scan.nextLine(); // Grabs the input and saves it as string 'GrabbedURL'
-        BigBoi.URLReader(GrabbedNameAdd); // Calls the URLReader Method with the entered URL as the input
+        String grabbedNameAdd = BigBoi.scan.nextLine(); // Grabs the input and saves it as string 'GrabbedURL'
+        BigBoi.websiteReader(grabbedNameAdd); // Calls the URLReader Method with the entered URL as the input
     }
 
-    public void URLReader(String ImportedName) throws Exception{
+    public void websiteReader(String importedName) throws Exception{
         // Setter URLReader method Takes the URL and pulls in the raw HTML website data
         // Then Searches the data line by line until its found the name
-        URL ECS_Website = new URL("https://www.ecs.soton.ac.uk/people/"+ImportedName);
-        String Websites = ("https://www.ecs.soton.ac.uk/people/"+ImportedName);
-        System.out.println(Websites);
-        //Creat a new URL object called ECS_Website using the users entered Email ID
+        URL eCS_Website = new URL("https://www.ecs.soton.ac.uk/people/"+importedName);
+        String websiteUrlString = ("https://www.ecs.soton.ac.uk/people/"+importedName);
+        // System.out.println(websiteUrlString); For Debugging
+        //Creat a new URL object called eCS_Website using the users entered Email ID
 
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(ECS_Website.openStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(eCS_Website.openStream()));
         // Initialise  a buffered reader object using the URL object
-        String InputStream;
-        while ((InputStream = in.readLine()) != null){
-            if(InputStream.contains("name") || InputStream.contains(URLNameGrabber(Websites))){
+        String inputStream;
+        while ((inputStream = in.readLine()) != null){
+            if(inputStream.contains("name") || inputStream.contains(urlNameGrabber(websiteUrlString))){
                 // Filters the raw html to only save lines of code that contain the word name or the name in the email
-                Pattern PatLookingFor = Pattern.compile( "<h1 class=\"uos-page-title uos-main-title uos-page-title-compressed\" property=\"name\">(.*?)<em property=\"honorificSuffix\">&nbsp;</em></h1>" );
+                Pattern patLookingFor = Pattern.compile( "<h1 class=\"uos-page-title uos-main-title uos-page-title-compressed\" property=\"name\">(.*?)<em property=\"honorificSuffix\">&nbsp;</em></h1>" );
                 // Pattern object which is used by me to filter the code down to just the name using some regex
-                Matcher m = PatLookingFor.matcher(InputStream);
+                Matcher m = patLookingFor.matcher(inputStream);
                 if( m.find() ) {
-                    String FinalName = m.group(1);
-                    System.out.println(FinalName);
+                    String finalName = m.group(1);
+                    System.out.println(finalName);
                     break;
                 }
 
@@ -48,19 +48,23 @@ public class Main {
         } // Loop runs until all the code has been passed through, being filtered out by the if statements
     }
 
-    public String URLNameGrabber(String CurrentURL){ // Method grabs the name off the end of the url to be used with filtering
-        String EndOfURL = "0";
+
+
+
+    public String urlNameGrabber(String CurrentURL){ // Method grabs the name off the end of the url to be used with filtering (Not needed)
+        // This method is only useful if the user gives you a url and not the email ID
+        String endOfURL = "0";
         try {
             URI uri = new URI(CurrentURL);
             String path = uri.getPath();
-            EndOfURL = path.substring(path.lastIndexOf('/') + 1);
+            endOfURL = path.substring(path.lastIndexOf('/') + 1);
             // Creats a substring EndOfURL which holds the name used as the last path in the url
         }catch(URISyntaxException e){
             e.printStackTrace();
-            System.out.println("No Name In URL");
+            System.out.println("Nothing at end of URL");
         }
 
-        return EndOfURL;
+        return endOfURL;
     }
 
 }
